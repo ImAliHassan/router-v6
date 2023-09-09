5504022b0081dc89b8c5c3dc567038715f53621b
Mian Ahmad Basit <66495447+mianahmadbasit1@users.noreply.github.com>
2023-09-09 16:46:10 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# router-v6-
+ its all about updating of routes in react topic 
---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Home from './comp/home'
+import About from './comp/about'
+import Contact from './comp/contact'
+import Error from './comp/error'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route,Link } from 'react-router-dom';
+{
+  /* The following line can be included in your src/index.js or App.js file */
+}
+function App() {
+  return (
+<Router>
+
+    <div>
+    <h1>hi ahmad</h1>
+      <Link to={'/'} >Home</Link>
+      <Link to={'/about'} >About</Link>
+      <Link to={'/contact'} >Contact</Link>
+
+
+
+      <Routes>
+
+        <Route path='/' element={<Home/>}/>
+        
+        <Route path='/about' element={<About/>}/>
+      
+       <Route path='/contact' element={<Contact/>}/>
+
+       <Route path='*' element={<Error/>}/>
+
+      </Routes>
+      
+
+
+    </div>
+    </Router>
+
+  )
+}
+
+export default App
